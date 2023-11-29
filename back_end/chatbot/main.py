5d4ac0ebb9dd2c9cfcbdf05301f42d519c549cbc
yakamurii <30423462+yakamurii@users.noreply.github.com>
2023-11-29 09:32:00 -0300

    ajuste na tokenização, remoção da lematização e verificação de negação ou afirmação

--- back_end/chatbot/main.py
+++ back_end/chatbot/main.py
@@ -4,14 +4,15 @@ from processar_texto import processar_respostas
 
 if __name__=="__main__":
 
-
+    # Exemplo de respostas do paciente
     respostas_paciente = [
-        "Passei bem a noite e não senti febre.",
-        "Senti um pouco de dor na ferida.",
-        "A ferida está vermelha e com um pouco de pus."
+        "não está com pus",
     ]
 
+    
+    # Processar as respostas do paciente
     sintomas_identificados = processar_respostas(respostas_paciente)
 
+    # Mostrar os sintomas identificados
     print("Sintomas identificados:")
     print(sintomas_identificados)
