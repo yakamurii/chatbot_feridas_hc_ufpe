6eec269a7567a4ea2ce842b4cd33724d544037b0
yakamurii <30423462+yakamurii@users.noreply.github.com>
2023-11-29 09:36:52 -0300

    processamento das respostas usando tokenização

--- back_end/chatbot/main.py
+++ back_end/chatbot/main.py
@@ -1,18 +1,12 @@
 from processar_texto import processar_respostas
 
-
-
 if __name__=="__main__":
 
-    # Exemplo de respostas do paciente
     respostas_paciente = [
         "não está com pus",
     ]
 
-    
-    # Processar as respostas do paciente
     sintomas_identificados = processar_respostas(respostas_paciente)
 
-    # Mostrar os sintomas identificados
     print("Sintomas identificados:")
     print(sintomas_identificados)
--- back_end/chatbot/processar_texto.py
+++ back_end/chatbot/processar_texto.py
@@ -1,11 +1,10 @@
 import spacy
 
-# Carregar o modelo em português
 nlp = spacy.load("pt_core_news_sm")
 
-# Função para processar as respostas do paciente
+
 def processar_respostas(respostas):
-    negacoes = {"não", "nada", "nunca", "jamais"}  # Palavras que indicam negação
+    negacoes = {"não", "nada", "nunca", "jamais"}  
     sintomas = {
         "febre": False,
         "dor": False,
@@ -20,27 +19,23 @@ def processar_respostas(respostas):
 
         for i, token in enumerate(tokens):
             if token == "febre":
-                # Verificar se existem tokens de negação antes da palavra "febre"
                 negacao_antes_da_febr = any(prev_token in negacoes for prev_token in tokens[:i])
                 sintomas["febre"] = not negacao_antes_da_febr
 
             if token == "dor":
-                # Verificar se existem tokens de negação antes da palavra "febre"
+
                 negacao_antes_da_dor = any(prev_token in negacoes for prev_token in tokens[:i])
                 sintomas["dor"] = not negacao_antes_da_dor
 
             if token == "cicatrizada":
-                # Verificar se existem tokens de negação antes da palavra "febre"
                 negacao_antes_da_cicatrizada = any(prev_token in negacoes for prev_token in tokens[:i])
                 sintomas["ferida_cicatrizada"] = not negacao_antes_da_cicatrizada
 
             if token == "vermelha":
-                # Verificar se existem tokens de negação antes da palavra "febre"
                 negacao_antes_da_vermelha = any(prev_token in negacoes for prev_token in tokens[:i])
                 sintomas["ferida_vermelha"] = not negacao_antes_da_vermelha
 
             if token == "pus":
-                # Verificar se existem tokens de negação antes da palavra "febre"
                 negacao_antes_da_pus = any(prev_token in negacoes for prev_token in tokens[:i])
                 sintomas["ferida_pus"] = not negacao_antes_da_pus
 
